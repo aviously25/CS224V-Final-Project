bd497ab346eedf1fdf612a9973f484d1eb80fe19
Avi Udash <udashavi@gmail.com>
2024-11-21 16:26:51 -0800

    add basic rag setup

--- app.py
+++ app.py
@@ -1,22 +1,61 @@
 import streamlit as st
+
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_pinecone import PineconeVectorStore
+from langchain_together import TogetherEmbeddings
+from langchain.chains import RetrievalQAWithSourcesChain
+
+
+from pinecone.grpc import PineconeGRPC as Pinecone
+
 from dotenv import load_dotenv
 import os
+import json
+
+from pinecone_utils import load_index
 
 # Load environment variables
 load_dotenv()
 
-# Set TogetherAI API key
-# Replace 'userdata.get' with 'os.getenv' or similar if 'userdata' is not defined in your project
-os.environ["TOGETHER_API_KEY"] = os.getenv("TOGETHER_API_KEY")
+# Setup API Keys
+TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 # App Configuration
 title = "CS224V LectureBot 📚"
 st.set_page_config(page_title=title, page_icon="🤖")
 st.title(title)
+
+# Configure Pinecone client
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "cs224v-lecturebot"
+index = load_index(pc, index_name=index_name)
+
+# initialize PineconeVectorStore and embeddings
+together_embedding = TogetherEmbeddings(
+    model="togethercomputer/m2-bert-80M-8k-retrieval"
+)
+vectorstore = PineconeVectorStore(index, embedding=together_embedding, text_key="text")
+
+
+def format_docs(docs):
+    print(
+        "\n\n".join(
+            [
+                json.dumps({"content": doc.page_content, "metadata": doc.metadata})
+                for doc in docs
+            ]
+        )
+    )
+    return "\n\n".join(
+        [
+            json.dumps({"content": doc.page_content, "metadata": doc.metadata})
+            for doc in docs
+        ]
+    )
 
 
 # Function to Stream Responses
@@ -25,14 +64,16 @@ def stream_response(user_query, chat_history):
     Streams response from TogetherAI using LangChain's ChatOpenAI wrapper.
     """
     template = """
-    Use the following pieces of context to answer the question at the end, and respond kindly.
+    Use the following pieces of context and chat history to answer the question at the end, and respond kindly.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Keep the answer as concise as possible.
+    Keep the answer concise.
     Always say "thanks for asking!" at the end of the answer.
 
     Chat history: {chat_history}
 
     User question: {user_question}
+
+    Retrieved context: {retrieved_context}
     """
 
     # Prepare the prompt
@@ -41,10 +82,16 @@ def stream_response(user_query, chat_history):
     # Initialize ChatOpenAI for TogetherAI
     llm = ChatOpenAI(
         base_url="https://api.together.xyz/v1",
-        api_key=os.environ["TOGETHER_API_KEY"],
+        api_key=TOGETHER_API_KEY,
         model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
         streaming=True,
     )
+
+    retriever = vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 20}
+    )
+    retrieved_context = retriever.invoke(user_query)
+    retrieved_context = format_docs(retrieved_context)
 
     # Combine prompt and TogetherAI in LangChain pipeline
     chain = prompt | llm | StrOutputParser()
@@ -52,8 +99,9 @@ def stream_response(user_query, chat_history):
     # Stream response
     return chain.stream(
         {
-            "chat_history": chat_history,
+            "retrieved_context": retrieved_context,
             "user_question": user_query,
+            "chat_history": chat_history,
         }
     )
 
@@ -81,10 +129,7 @@ if user_query:
     with st.chat_message("Human"):
         st.markdown(user_query)
 
-    # TODO: do RAG stuff here
-
     # Stream the LLM Response
-    # TODO: input context
     with st.chat_message("AI"):
         response_stream = stream_response(user_query, st.session_state.chat_history)
         response = st.write_stream(response_stream)
